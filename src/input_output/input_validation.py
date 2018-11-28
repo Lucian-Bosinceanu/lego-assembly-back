8f5165b97a6f994613651f86f9cc79f5026d23f1
Feciuc Stelian <stelian@teramind.co>
2018-11-28 16:57:21 +0200

    -Solved a few conflicts

--- src/input_output/input_validation.py
+++ src/input_output/input_validation.py
@@ -1,5 +1,4 @@
 import sys
-<<<<<<< HEAD
 import string
 
 def input_validation(shape):
@@ -17,31 +16,6 @@ def input_validation(shape):
                     if not(all(c in string.hexdigits for c in shape.cubes[x][y][z].color[1:])):
                       sys.exit("A cube's color field is not in HEXA")  
     
-=======
-
-visited = dict()
-
-def checkIfConex(cubes, poz):
-    if poz in visited:
-        return 0
-    visited[poz] = True
-    visitedCubes = 1
-    for i in range (0, len(cubes)):
-        print(cubes[poz].distFrom(cubes[i].x, cubes[i].y, cubes[i].z))
-        if i != poz and cubes[poz].distFrom(cubes[i].x, cubes[i].y, cubes[i].z) == 1:
-            visitedCubes += checkIfConex(cubes, i)
-    return visitedCubes
-
-
-def input_validation(shape):
-    if len(shape.cubes) > 5000:
-        sys.exit("The amount of cubes must be <= 5000")
-    if checkIfConex(shape.cubes, 0) != len(shape.cubes):
-        sys.exit("The input doesn't have all its blocks conex")
-    for i in range(0, len(shape.cubes)):
-        if shape.cubes[i].y < 0:
-            sys.exit("The Y of a cube must always be > 0")
->>>>>>> c15c5771970befcbf58be89a7ec2034daef851d0
     return True
 
     
--- src/input_output/model.py
+++ src/input_output/model.py
@@ -3,15 +3,10 @@ class Model:
         self.cubes = cubes
     def __str__(self):
         modelToStr = ""
-<<<<<<< HEAD
         for x in self.cubes:
             for y in self.cubes[x]:
                 for z in self.cubes[x][y]:
                     modelToStr = modelToStr + "x=" + str(x) + " y=" + str(y) + " z=" + str(z) + " " + str(self.cubes[x][y][z]) + "\n"
-=======
-        for cube in self.cubes:
-            modelToStr = modelToStr + str(cube) + "\n"
->>>>>>> c15c5771970befcbf58be89a7ec2034daef851d0
         return modelToStr
     def __repr__(self):
         return str(self)
