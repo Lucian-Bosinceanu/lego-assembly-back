6d39e9cf4e666cdc74cb5e63fd3c7f7eb2f49073
timofteandrei <andrei.timofte@feel-it-services.com>
2018-12-02 19:29:10 +0200

    add graph_validation file structure and implement connected_graph function

--- src/graphs/graph_validation.py
+++ src/graphs/graph_validation.py
@@ -1,4 +1,4 @@
-from .graph import Graph
+# from .graph import Graph
 
 # The graph is considered valid if :
 # It is fully connnected
@@ -9,5 +9,41 @@ from .graph import Graph
 # where the logic for finding weak articulation points and such should be implemented
 
 
-def graph_validation(graph):
-    pass
+def graph_validation(connections):
+
+    def connected_graph():
+        visitedNodes = {}
+
+        def recursive_DFS(currentNode):
+            visitedNodes[currentNode] = True
+            for node in connections[currentNode]:
+                if (str(node) not in visitedNodes):
+                    recursive_DFS(str(node))
+
+        firstNode = connections.keys()[0]
+        recursive_DFS(firstNode)
+        #if number of visited nodes after DFS not equal
+        # number of nodes in graph ==> Disconnected graph
+        if (len(visitedNodes) != len(connections.keys())):
+           return False
+        return True
+
+    def weak_articulation_points():
+        return False
+
+    if connected_graph() == True and weak_articulation_points() == False:
+        return True
+    return False
+
+modelGraph = {
+    "1": [1, 2, 3],
+    "2": [1, 4],
+    "3": [1, 4, 5],
+    "4": [2, 3],
+    "5": [3, 6, 7, 8],
+    "6": [5],
+    "7": [5],
+    "8": [5]
+}
+print(graph_validation(modelGraph))
+# python src/graphs/graph_validation.py
