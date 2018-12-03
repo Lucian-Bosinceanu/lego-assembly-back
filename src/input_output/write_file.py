ff5af22f08b53b68f2cc11a968ec10a65ab1b409
cameliabacaoanu <bacaoanucamelia@yahoo.com>
2018-12-03 22:44:12 +0200

    add the rotation part

--- src/input_output/write_file.py
+++ src/input_output/write_file.py
@@ -6,10 +6,8 @@ from cubedata import CubeData
 
 def write_file():
   #import the standard pieces  
-  path= r"C:\Users\Roxana\Desktop\scLego.json"  
-  with open(path, 'r') as file:
+  with open("scLego.json", 'r') as file:
     standardPieces = json.load(file)
-  #print(standardPieces)
   cubes = {'1': {'1':  {'1':  CubeData('red',1)}},'2':{ '1': { '1': CubeData('red',1)} }} #a example of dictionary
   #create a new dictionary where the key is the id + color and the value is a tuple of coordinates
   dictionary= dict()
@@ -20,9 +18,9 @@ def write_file():
               dictionary[str(cubes[x][y][z].id)+'-'+cubes[x][y][z].color].append((x,y,z))  
           else:
               dictionary[str(cubes[x][y][z].id)+'-'+cubes[x][y][z].color]=[(x,y,z)]
-  #sort the tuples by y,x,z            
+  #sort the tuples by y,z,x            
   for key in dictionary:
-    dictionary[key].sort(key=lambda x: (x[2],x[0],x[1]))
+    dictionary[key].sort(key=lambda x: (x[1],x[2],x[0]))
   usedPieces= dict()
   usedPieces['pieces']=[]
   #iterate through the dictionary and find the standard pieces we use in the sculpture
@@ -33,22 +31,50 @@ def write_file():
        dic= {'x': 0, 'y':0,'z':0}
        coord= list()
        coord.append(dic)
+       rotation=''
        for i in range(1,len(dictionary[key])):
           a=abs(int(x)-int(dictionary[key][i][0]))
           b=abs(int(y)-int(dictionary[key][i][1]))
           c=abs(int(z)-int(dictionary[key][i][2]))
           dic={'x':a,'y':b,'z':c}
           coord.append(dic)
+       if(a<c):
+         rotation="90"
+       else:
+         rotation="0"
        for piece in range(0,len(standardPieces["pieces"])):
            if(coord == standardPieces["pieces"][piece]["structure"]):
               if(standardPieces["pieces"][piece] not in usedPieces["pieces"]):
                 usedPieces['pieces'].append(standardPieces["pieces"][piece])   
-              dictionary[key+'-'+standardPieces["pieces"][piece]["name"]] = dictionary.pop(key)
+              dictionary[key+'-'+standardPieces["pieces"][piece]["name"]+'-'+rotation] = dictionary.pop(key)
               
-  print(usedPieces)
-  print(dictionary) 
+ #sort the tuples by y,x,z  
+  for key in dictionary:
+    dictionary[key].sort(key=lambda x: (x[1],x[0],x[2]))
   
   orderPieces= dict()
   orderPieces['order']=[]
+
+#getting the assembling order of pieces 
+  for key in dictionary:
+      v=key.split('-')
+      output=dict()
+      output["piece-name"]=v[2]
+      poz=dict()
+      poz["x"]=dictionary[key][0][0]
+      poz["y"]=dictionary[key][0][1]
+      poz["z"]=dictionary[key][0][2]
+      output["position"]=poz
+      orderPieces['order'].append(output)
+      output["rotation"]=v[3]
+      output["color"]=v[1]
+
+ #union of the first two dictionaries 
+  usedPieces.update(orderPieces)
+
+  json_string = json.dumps(usedPieces)
+  JsonOutput = open("JsonOutput.json","w+") 
+  JsonOutput.write(json_string) 
+  JsonOutput.close() 
 
 write_file()
