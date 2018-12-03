d5e117ed0da2b529ecc5ad9b2b4fb666d92f714a
Mihai Bojescu <bojescu.mihai@gmail.com>
2018-12-04 00:39:57 +0200

    Integrate input with cube merger

--- src/generation/merge.py
+++ src/generation/merge.py
@@ -1,3 +1,7 @@
+from operator import itemgetter
+import src.input_output.model as model
+import src.input_output.cubedata as cube
+
 SHAPES = [
     {
         'id': 0,
@@ -36,39 +40,11 @@ SHAPES = [
     }
 ]
 
-voxels = [
-    {
-        "id": 0,
-        "shape": None,
-        "position": (0, 0, 0)
-    },
-    {
-        "id": 1,
-        "shape": None,
-        "position": (1, 0, 0)
-    },
-    {
-        "id": 2,
-        "shape": None,
-        "position": (0, 0, 2)
-    }, 
-    {
-        "id": 3,
-        "shape": None,
-        "position": (0, 0, 5)
-    },
-    {
-        "id": 4,
-        "shape": None,
-        "position": (2, 0, 1)
-    }
-]
-
-def search(x, y, z):
+def search(voxels, x, y, z):
     tuple_to_compare_to = (x, y, z)
-    return next((x for x in voxels if x["position"] == tuple_to_compare_to), None)
+    return next((x for x in voxels if x["position"] == tuple_to_compare_to and x["shape"] is None), None)
 
-def classify(x, y, z):
+def classify(voxels, x, y, z):
     rotations = ["0 deg", "90 deg", "180 deg", "270 deg"]
     for shape in SHAPES[::-1]:
         max_x = max(shape["like"], key=lambda x: x[0])[0]
@@ -76,10 +52,10 @@ def classify(x, y, z):
         max_z = max(shape["like"], key=lambda x: x[2])[2]
         variations = [
             [
-                [search(x + position[0] - xprime, y + position[1] - yprime, z + position[2] - zprime) for position in shape["like"]],
-                [search(x + position[2] - xprime, y + position[1] - yprime, z + position[0] - zprime) for position in shape["like"]],
-                [search(x - position[0] - xprime, y + position[1] - yprime, z - position[2] - zprime) for position in shape["like"]],
-                [search(x - position[2] - xprime, y + position[1] - yprime, z - position[0] - zprime) for position in shape["like"]]
+                [search(voxels, x + position[0] - xprime, y + position[1] - yprime, z + position[2] - zprime) for position in shape["like"]],
+                [search(voxels, x + position[2] - xprime, y + position[1] - yprime, z + position[0] - zprime) for position in shape["like"]],
+                [search(voxels, x - position[0] - xprime, y + position[1] - yprime, z - position[2] - zprime) for position in shape["like"]],
+                [search(voxels, x - position[2] - xprime, y + position[1] - yprime, z - position[0] - zprime) for position in shape["like"]]
             ]
             for xprime in range(0, max_x + 1) for yprime in range(0, max_y + 1) for zprime in range(0, max_z + 1)
         ]
@@ -90,3 +66,30 @@ def classify(x, y, z):
         )
         if result is not None:
             return result
+
+def merge_cubes(model: model):
+    voxels = []
+    for x in model.cubes:
+        for y in model.cubes[x]:
+            for z in model.cubes[x][y]:
+                voxels.append({
+                    "id": model.cubes[x][y][z].id,
+                    "color": model.cubes[x][y][z].color,
+                    "shape": None,
+                    "position": (x, y, z),
+                    "reference": model.cubes[x][y][z]
+                })
+
+    voxels.sort(key=itemgetter("position"))
+    recognisedShapes = []
+    for voxel in voxels:
+        if voxel["shape"] is None:
+            result = classify(voxels, voxel["position"][0], voxel["position"][1], voxel["position"][2])
+            if result is not None:
+                for recognisedVoxel in result[2]:
+                    recognisedVoxel["shape"] = result[0]["id"]
+                recognisedShapes.append(result)
+
+    return recognisedShapes
+
+            
