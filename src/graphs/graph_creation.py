795716559362b78a239047211c4eaf264fbce580
crivoi <andrei.crivoi1997@gmail.com>
2018-12-03 23:01:17 +0200

    Graph creation now suits Model

--- src/graphs/graph_creation.py
+++ src/graphs/graph_creation.py
@@ -10,26 +10,15 @@ def graph_creation(merged):
 
     connections = defaultdict(list)
 
-    # for i in merged.cubes:
-    #     for j in merged.cubes[i]:
-    #         for k in merged.cubes[i][j]:
-    #             id = merged.cubes[i][j][k].id
-    #             if id not in connections.keys():
-    #                 try:
-    #                     connections[id].append(merged.cubes[i][j][k - 1].id)
-    #                 except:
-    #                     pass
-
-    for piece in merged.pieces:
-        for i in piece.cubes:
-            for j in piece.cubes[i]:
-                for k in piece.cubes[i][j]:
-                    id = piece.cubes[i][j][k].id
-                    if id not in connections.keys():
-                        try:
-                            connections[id].append(piece.cubes[i][j][k - 1].id)
-                        except:
-                            pass
+    for i in merged.cubes:
+        for j in merged.cubes[i]:
+            for k in merged.cubes[i][j]:
+                id = merged.cubes[i][j][k].id
+                if id not in connections.keys():
+                    try:
+                        connections[id].append(merged.cubes[i][j][k - 1].id)
+                    except:
+                        pass
 
     # print(str(connections.keys()) + " " + str(connections.items()))
 
