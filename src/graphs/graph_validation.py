4c9f6aa0b1a57b22be15bf0fd19b5955d5f0b185
Andrei <casuneanu.andrei97@gmail.com>
2018-12-03 21:26:08 +0200

    added weak points verification

--- src/graphs/graph_validation.py
+++ src/graphs/graph_validation.py
@@ -9,41 +9,66 @@
 # where the logic for finding weak articulation points and such should be implemented
 
 
-def graph_validation(connections):
+def graph_validation(connections, limit):
+    connectionsKeys = list(connections)
 
-    def connected_graph():
+    def DFS(startingNode, ignore = -1):
         visitedNodes = {}
 
         def recursive_DFS(currentNode):
             visitedNodes[currentNode] = True
             for node in connections[currentNode]:
-                if (str(node) not in visitedNodes):
-                    recursive_DFS(str(node))
+                if node == ignore: continue
+                if (node not in visitedNodes):
+                    recursive_DFS(node)
+        
+        recursive_DFS(startingNode)
+        return visitedNodes
 
-        firstNode = connections.keys()[0]
-        recursive_DFS(firstNode)
+    def connected_graph():
+        firstNode = connectionsKeys[0]
+        visitedNodes = DFS(firstNode)
         #if number of visited nodes after DFS not equal
         # number of nodes in graph ==> Disconnected graph
-        if (len(visitedNodes) != len(connections.keys())):
+        if (len(visitedNodes) != len(connectionsKeys)):
            return False
         return True
 
     def weak_articulation_points():
-        return False
+        weakPoints = []
 
-    if connected_graph() == True and weak_articulation_points() == False:
+        visitedNodes = DFS(connectionsKeys[1], connectionsKeys[0])
+        if len(visitedNodes) != len(connectionsKeys) - 1:
+            weakPoints.append(connectionsKeys[0])
+
+        currentNode = 1
+        while currentNode < len(connectionsKeys):
+            visitedNodes = DFS(1, connectionsKeys[currentNode])
+            if len(visitedNodes) != len(connectionsKeys) - 1:
+                weakPoints.append(connectionsKeys[currentNode])
+            currentNode += 1
+
+        print(weakPoints)
+        if len(weakPoints) > limit:
+            return False
+        return True
+
+    if connected_graph() == True and weak_articulation_points() == True:
         return True
     return False
 
 modelGraph = {
-    "1": {1, 2, 3},
-    "2": {1, 4},
-    "3": {1, 4, 5},
-    "4": {2, 3},
-    "5": {3, 6, 7, 8},
-    "6": {5},
-    "7": {5},
-    "8": {5}
+    1: {1, 2, 3, 9},
+    2: {1, 4, 9},
+    3: {1, 4, 5},
+    4: {2, 3},
+    5: {3, 6, 7, 8},
+    6: {5},
+    7: {5},
+    8: {5},
+    9: {
+        1, 2
+    }
 }
-print(graph_validation(modelGraph))
+print(graph_validation(modelGraph, 3))
 # python src/graphs/graph_validation.py
