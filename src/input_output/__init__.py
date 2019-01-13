65015e3c7b71e3ca0f100fc4e189cae3c3a253dc
Mihai Bojescu <bojescu.mihai@gmail.com>
2019-01-14 00:06:23 +0200

    Implement algorithm selection, fix greedy algorithm

--- src/input_output/__init__.py
+++ src/input_output/__init__.py
@@ -3,5 +3,6 @@ from .input_validation import input_validation
 from .write_file import write_file
 from .model import Model
 from .cubedata import CubeData
+from .arguments import arguments
 
-__all__ = ["read_file", "input_validation", "write_file", "Model", "CubeData"]
+__all__ = ["read_file", "input_validation", "write_file", "Model", "CubeData", "arguments"]
