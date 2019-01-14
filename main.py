ad728b5a6479ba844a6edb316ef544f699503161
Mihai Bojescu <bojescu.mihai@gmail.com>
2019-01-15 00:39:00 +0200

    Remove algorithm selection

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.generation import *
 from src.generation.new_merge import *
 
 if __name__ == "__main__":
-    algorithm = arguments()
+    # algorithm = arguments()
     shape = Model(read_file())
     input_validation(shape)
     print(shape)
@@ -19,8 +19,9 @@ if __name__ == "__main__":
     graph = None
 
     while not done and attempts < max_attempts:             # while the final structure still isn't right
-        if algorithm == "genetic": merge_test(shape)        # merge the input from file, creating a basic lego structure
-        else: merge_cubes(shape)                            # merge the input using the greedy algorithm
+        # if algorithm == "genetic": merge_test(shape)      # merge the input from file, creating a basic lego structure
+        # else: merge_cubes(shape)                          # merge the input using the greedy algorithm
+        merge_test(shape)                                   # merge the input from file, creating a basic lego structure
         graph = graph_creation(shape)                       # remove weak articulation points, merge eventual subgraphs
         done = graph_validation(graph.connections, limit)   # if graph is stable, we can stop
         attempts += 1
